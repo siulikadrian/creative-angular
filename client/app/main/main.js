27a330c46db70886f81545a5e814425267dbebb3
Adrian Siulik <adriansiulik@gmail.com>
2014-09-24 22:20:16 +0200

    first commit

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('creativeRecruitmentApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
